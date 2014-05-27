770cfd7e2ac9685fa3602ffb91b08b18686cb8aa
bengrunfeld-wf <ben.grunfeld@webfilings.com>
2014-05-27 15:02:47 -0600

    Write tests for auth.py

--- console/tests/test_auth.py
+++ console/tests/test_auth.py
@@ -1,53 +1,38 @@
 """
 Test all classes and functions in auth.py
-""" 
+"""
 
 import unittest
 
-from mock import Mock
-from mock import patch
-
-from google.appengine.api import urlfetch_service_pb
-from google.appengine.ext import testbed
+from mock import MagicMock
 
 
-# class BaseTestUrlFetch(unittest.TestCase):
-#     """Base class for any class using URL Fetch"""
-# 
-#     def setUp(self):
-#         self.testbed = testbed.Testbed()
-#         self.testbed.activate()
-#         self.testbed.init_urlfetch_stub()
-# 
-#     def tearDown(self):
-#         self.testbed.deactivate()
+class TestAuthFunctions(unittest.TestCase):
+    """Test the global functions in auth.py"""
 
-
-class TestUrlFetch(unittest.TestCase):
-    """Test if fetch_url sending legitimate requests"""
+    # Define the url and the response
+    url = 'https://google.com'
+    response = 'Test response'
 
     def test_fetch_url(self):
+        """Test the fetch_url function"""
+
         from console.auth import fetch_url
 
-        # Mock the fetch_url function 
-        mock = Mock(wrap=fetch_url)
-        content = mock.fetch_url.return_value = ('https://google.com')
+        # Mock the fetch_url function
+        mock_fetch_url = MagicMock(spec=fetch_url, return_value=self.response)
 
-        # Test that content is not empty
-        self.assertIsNotNone(content)
+        # Test the function
+        assert mock_fetch_url(self.url) == self.response
 
+    def test_get_user_name(self):
+        """Test the get_user_name funciton in auth.py"""
 
-# class TestGetUserName(unittest.TestCase):
-#     """Test that a valid username is being returned"""
-# 
-#     def test_get_username(self):
-#         from console.auth import get_user_name
-# 
-#         # Request username from GitHub 
-#         content = get_user_name()
-# 
-#         # Print content
-#         print(content)
-# 
-#         # Test that response does not equal error
-#         assertIsNotNone(content)
+        from console.auth import get_user_name
+
+        # Mock the get_user_name function
+        mock_get_user_name = MagicMock(spec=get_user_name,
+                                       return_value=self.response)
+
+        # Test the function
+        assert mock_get_user_name() == self.response
