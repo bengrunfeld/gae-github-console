20b931ac689b2723e858e6327a9446a86af8d290
bengrunfeld-wf <ben.grunfeld@webfilings.com>
2014-05-29 13:03:04 -0600

    Add tests for global functions in auth

--- console/tests/test_auth.py
+++ console/tests/test_auth.py
@@ -30,27 +30,66 @@ class TestAuthFunctions(unittest.TestCase):
 
         from console.auth import get_user_name
 
-        # Mock the get_user_name function
         mock_get_user_name = MagicMock(spec=get_user_name,
                                        return_value=self.response)
 
-        # Test the function
         assert mock_get_user_name() == self.response
 
+    def test_create_flow_object(self):
+        from console.auth import _create_flow_object
+
+        mock_create_flow = MagicMock(spec=_create_flow_object,
+                                     return_value=self.response)
+
+        assert mock_create_flow() == self.response
+
+    def test_get_auth_uri(self):
+        from console.auth import _get_auth_uri
+
+        mock_get_auth_uri = MagicMock(spec=_get_auth_uri,
+                                      return_value=self.response)
+
+        assert mock_get_auth_uri() == self.response
+
     def test_retrieve_code(self):
-        pass
+        from console.auth import _retrieve_code
+
+        mock_retrieve_code = MagicMock(spec=_retrieve_code,
+                                       return_value=self.response)
+
+        assert mock_retrieve_code() == self.response
 
     def test_get_org_name(self):
-        pass
+        from console.auth import _get_org_name
+
+        mock_get_org_name = MagicMock(spec=_get_org_name,
+                                      return_value=self.response)
+
+        assert mock_get_org_name() == self.response
 
     def test_user_is_org_member(self):
-        pass
+        from console.auth import _user_is_org_member
+
+        test_user_is_org_member = MagicMock(spec=_user_is_org_member,
+                                            return_value=self.response)
+
+        assert test_user_is_org_member() == self.response 
 
     def test_user_is_org_admin(self):
-        pass
+        from console.auth import _user_is_org_admin
+
+        mock_user_is_org_admin = MagicMock(spec=_user_is_org_admin,
+                                            return_value=self.response)
+
+        assert mock_user_is_org_admin() == self.response
 
     def test_check_app_config(self):
-        pass
+        from console.auth import _check_app_config
+
+        mock_check_app_config = MagicMock(spec=_check_app_config,
+                                          return_value=self.response)
+
+        assert mock_check_app_config() == self.response
 
 
 class TestAuthStorageFunctions(unittest.TestCase):
